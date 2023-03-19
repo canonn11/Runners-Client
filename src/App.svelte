d69a09f0fc441529c2ecfc3103a6c6045053374c
canonn11 <myungkwanokk@gmail.com>
2023-03-19 23:26:48 +0900

    tailwind setup

--- src/App.svelte
+++ src/App.svelte
@@ -5,8 +5,11 @@
 </script>
 
 <main>
-  <a href="/">홈</a>
+  <a href="/" class="text-2xl">홈</a>
   <a href="/#/index">어바웃</a>
+
+  <h1 class="text-3xl font-bold underline">This is Home</h1>
+
 </main>
 
 <Router
